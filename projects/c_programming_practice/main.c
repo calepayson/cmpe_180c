c1d1f889bf87e913480ffe242470a9f7571447a3
Cale Smith <calepayson@mac.com>
2025-02-18 20:27:12 -0800

    Add c_programming_practice/
    - My first programming project!
    - Parses command-line arguments from the user and spits out lottery
      numbers.

---
+++ projects/c_programming_practice/main.c
@@ -0,0 +1,117 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct {
+    int min;
+    int max;
+} Range;
+
+typedef struct {
+    int value;
+    bool required;
+    bool provided;
+    Range range;
+    char *name;
+} Input;
+
+const Range NUMS_TO_GEN_RANGE = {1, 100};
+const Range NUMS_RANGE = {1, 99};
+const Range POWER_BALL_RANGE = {1, 99};
+const Range NUM_SETS_RANGE = {1, 32767};
+
+void validate_input(Input *input);
+
+int rand_in_range(int min, int max);
+
+int main(int argc, char *argv[]) {
+    Input numbers_to_generate = {0, true, false, NUMS_TO_GEN_RANGE,
+                                 "-n <NumbersToGenerate>"};
+    Input max_number = {0, true, false, NUMS_RANGE, "-r <MaxNumber>"};
+    Input max_power_ball_number = {0, false, false, POWER_BALL_RANGE,
+                                   "-p <MaxPowerBallNumber"};
+    Input number_sets_to_generate = {1, false, false, NUM_SETS_RANGE,
+                                     "-N <NumberSetsToGenerate"};
+
+    // Read in the arguments
+    int opt;
+    while ((opt = getopt(argc, argv, "n:r:p:N:")) != -1) {
+        switch (opt) {
+            case 'n':
+                numbers_to_generate.value = atoi(optarg);
+                numbers_to_generate.provided = true;
+                break;
+            case 'N':
+                number_sets_to_generate.value = atoi(optarg);
+                number_sets_to_generate.provided = true;
+                break;
+            case 'p':
+                max_power_ball_number.value = atoi(optarg);
+                max_power_ball_number.provided = true;
+                break;
+            case 'r':
+                max_number.value = atoi(optarg);
+                max_number.provided = true;
+                break;
+            default:
+                fprintf(stderr,
+                        "Usage: %s -n <NumbersToGenerate> -r <MaxNumber> [-p "
+                        "<MaxPowerBallNumber>] [-N <NumberSetsToGenerate>]",
+                        argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    validate_input(&numbers_to_generate);
+    validate_input(&max_number);
+    validate_input(&max_power_ball_number);
+    validate_input(&number_sets_to_generate);
+
+    for (int i = 0; i < number_sets_to_generate.value; i++) {
+        for (int j = 0; j < numbers_to_generate.value; j++) {
+            int num = rand_in_range(max_number.range.min, max_number.value);
+            if (!max_power_ball_number.provided &&
+                j + 1 == numbers_to_generate.value) {
+                printf("%i", num);
+            } else {
+                printf("%i, ", num);
+            }
+        }
+        if (max_power_ball_number.provided) {
+            int num = rand_in_range(max_power_ball_number.range.min,
+                                    max_power_ball_number.value);
+            printf("%i", num);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+void validate_input(Input *input) {
+    if (input->required) {
+        if (!input->provided) {
+            fprintf(stderr, "You must provide a positive integer for %s.\n",
+                    input->name);
+            exit(EXIT_FAILURE);
+        }
+        if (input->value == 0) {
+            fprintf(stderr,
+                    "Error parsing %s. Please provide a positive integer.\n",
+                    input->name);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (input->provided &&
+        (input->value < input->range.min || input->value > input->range.max)) {
+        fprintf(stderr,
+                "%s must be a value within [%i, %i].\n"
+                "You provided: %i\n",
+                input->name, input->range.min, input->range.max, input->value);
+        exit(EXIT_FAILURE);
+    }
+}
+
+int rand_in_range(int min, int max) { return min + rand() % (max - min + 1); }
