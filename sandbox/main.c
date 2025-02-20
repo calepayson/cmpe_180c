e2ea25bf2a6a1579700b8f7f073d8a1348ec6836
Cale Smith <calepayson@mac.com>
2025-02-19 19:29:31 -0800

    Modify sandbox/

--- sandbox/main.c
+++ sandbox/main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int rand_range(int min, int max);
+const int ARR_SIZE = 5;
 
 int main() {
-    int num = 1;
+    int arr[] = {1, 2, 3, 4, 5};
 
-    while (num != 0) {
-        num = rand_range(1, 10);
-        printf("Num = %i\n", num);
+    for (int i = 0; i < ARR_SIZE; i++) {
+        printf("%i, ", arr[i]);
     }
+
+    printf("\n");
 
     return 0;
 }
-
-int rand_range(int min, int max) { return min + rand() % (max - min + 1); }
